12b03c36f2018a5cfe022413a5c9a16ed22213b6
Nicola Ferru <66777517+NF02@users.noreply.github.com>
2020-06-12 21:05:22 +0200

    Create  keyWorld.h

---
+++ lib/keyWorld.h
@@ -0,0 +1,23 @@
+/* Video controls */
+#define XF86WebCam				0x1008FF8F   /* Launch web camera app. */
+#define XF86SplitScreen			0x1008FF7D   /* Split window or screen */
+
+/* Audio controls */
+#define XF86AudioMicMute		0x1008FFB2   /* Mute the Mic from the system */
+#define XF86AudioLowerVolume	0x1008FF11   /* Volume control down          */
+#define XF86AudioMute			0x1008FF12   /* Mute sound from the system   */
+#define XF86AudioRaiseVolume	0x1008FF13   /* Volume control up            */
+
+/* Luminosità dello schermo */
+#define XF86MonBrightnessUp     0x1008FF02   /* Monitor/panel brightness */
+#define XF86MonBrightnessDown   0x1008FF03   /* Monitor/panel brightness */
+#define XF86ScreenSaver			0x1008FF2D   /* Invoke screensaver       */
+#define XF86Display				0x1008FF59
+
+#define XF86AudioPlay			0x1008FF14
+
+#define XF86AudioStop			0x1008FF15
+#define XF86AudioPrev			0x1008FF16
+#define	XF86AudioNext			0x1008FF17
+#define XF86AudioPause			0x1008FF31
+
