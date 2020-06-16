cddbfe9cf75af15fdc81b9cb8b6118a9681062e5
Nicola Ferru <66777517+NF02@users.noreply.github.com>
2020-06-16 13:06:56 +0200

    Update config.h

--- config.h
+++ config.h
@@ -48,6 +48,8 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "TTT",      bstack },
 	{ "===",      bstackhoriz },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 
 };
 
@@ -118,6 +120,8 @@ static Key keys[] = {
 	{ MODKEY,                       			XK_m,				setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       			XK_o,		      		setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       			XK_u,				setlayout,      {.v = &layouts[4]} },
+	{ MODKEY|ShiftMask,					XK_u,				setlayout,      {.v = &layouts[5]} },
+	{ MODKEY|ShiftMask,					XK_o,				setlayout,      {.v = &layouts[6]} },					
 	{ MODKEY|ShiftMask,					XK_f,				fullscreen,     {0} },
 	{ MODKEY|ShiftMask,             			XK_space,			togglefloating, {0} },
 	{ MODKEY,                       			XK_0,				view,           {.ui = ~0 } },
