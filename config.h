89fd67603ae09b7cdb146cca91122a02f64a815b
Nicola Ferru <66777517+NF02@users.noreply.github.com>
2020-06-17 11:27:02 +0200

    Update config.h

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ static const Rule rules[] = {
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-
+#include "lib/fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
@@ -52,7 +52,6 @@ static const Layout layouts[] = {
 	{ ">M>",      centeredfloatingmaster },
 	{ "[@]",	  spiral },
 	{ "[\\]",	  dwindle },
-
 
 };
 
@@ -73,7 +72,7 @@ static const Layout layouts[] = {
 #include "lib/keyWorld.h"
 
 static Key keys[] = {
-	/* modifier                                             key		                function        argument */
+	/* modifier                     key		        function        argument */
 	/* 					Apps					*/
 	{ MODKEY,						XK_Insert,			spawn,         	{.v = clipcmd } },
 	{ MODKEY|ShiftMask,					XK_Insert,		        spawn,         	{.v = passmenu } },
@@ -123,11 +122,11 @@ static Key keys[] = {
 	{ MODKEY,                       			XK_m,				setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       			XK_o,		      		setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       			XK_u,				setlayout,      {.v = &layouts[4]} },
-	{ MODKEY|ShiftMask,					XK_u,				setlayout,      {.v = &layouts[5]} },
-	{ MODKEY|ShiftMask,					XK_o,				setlayout,      {.v = &layouts[6]} },
-	{ MODKEY|ShiftMask,                       		XK_o,				setlayout,      {.v = &layouts[6]} },
-	{ MODKEY,						XK_n,				setlayout,      {.v = &layouts[7]} },
-	{ MODKEY|ShiftMask,					XK_f,				fullscreen,     {0} },
+	{ MODKEY|ShiftMask,                       	XK_u,				setlayout,      {.v = &layouts[5]} },
+	{ MODKEY|ShiftMask,                       	XK_o,				setlayout,      {.v = &layouts[6]} },
+	{ MODKEY,									XK_n,				setlayout,      {.v = &layouts[7]} },
+	{ MODKEY|ShiftMask,							XK_n,				setlayout,      {.v = &layouts[8]} },
+	{ MODKEY|ShiftMask,							XK_f,				fullscreen,     {0} },
 	{ MODKEY|ShiftMask,             			XK_space,			togglefloating, {0} },
 	{ MODKEY,                       			XK_0,				view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             			XK_0,				tag,            {.ui = ~0 } },
@@ -163,4 +162,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
