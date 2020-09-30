12b1e8199343cf8da122f57937a6de69e8ebde03
NFVblog <66777517+NF02@users.noreply.github.com>
2020-09-30 18:05:52 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Terminus:style=Regular:pixelsize=14:antialias=true:autohint=true", "JoyPixels:pixelsize=11:antialias=true:autohint=true" };
@@ -45,7 +45,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 #include "lib/layout.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
