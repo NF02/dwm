a549fae407fc503c23065d3df59cd4a7d223574a
NFVblog <66777517+NF02@users.noreply.github.com>
2020-11-12 11:18:45 +0100

    Color fix

--- dwm.c
+++ dwm.c
@@ -1064,12 +1064,12 @@ loadxrdb()
       xrdb = XrmGetStringDatabase(resm);
 
       if (xrdb != NULL) {
-        XRDB_LOAD_COLOR("dwm.color0", normbordercolor);
-        XRDB_LOAD_COLOR("dwm.color8", normbgcolor);
-        XRDB_LOAD_COLOR("dwm.color0", normfgcolor);
+        XRDB_LOAD_COLOR("dwm.color4", normbordercolor);
+        XRDB_LOAD_COLOR("dwm.color0", normbgcolor);
+        XRDB_LOAD_COLOR("dwm.color2", normfgcolor);
         XRDB_LOAD_COLOR("dwm.color4", selbordercolor);
-        XRDB_LOAD_COLOR("dwm.color0", selbgcolor);
-        XRDB_LOAD_COLOR("dwm.color4", selfgcolor);
+        XRDB_LOAD_COLOR("dwm.color4", selbgcolor);
+        XRDB_LOAD_COLOR("dwm.color0", selfgcolor);
       }
     }
   }
