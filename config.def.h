2951a821d56daabda9f87a236f734742066dfc3a
nf02 <nicoferru190@gmail.com>
2023-04-15 13:22:49 +0200

    fix config

--- config.def.h
+++ config.def.h
@@ -1,5 +1,4 @@
 /* See LICENSE file for copyright and license details. */
-
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
@@ -7,15 +6,17 @@ static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char normbgcolor[]     = "#222222";
+static const char normbordercolor[] = "#444444";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char selfgcolor[]      = "#770000";
+static const char selbordercolor[]  = "#eeeeee";
+static const char selbgcolor[]      = "#005577";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -37,15 +38,22 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#include "lib/layout.h"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "[D]",      deck },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -53,15 +61,18 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/zsh", "-c", cmd, NULL } }
+#define TERMCMD(cmd) { .v = (const char*[]){ "st", "-e", cmd, NULL } }
+/* terminal */
+#define TERM "st"
+#define TMUX "st -e tmux"
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
 
+#include "lib/command.h"
+#include "lib/keyWorld.h"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_Print,  spawn,	   {.v = screenshot } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -77,6 +88,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_n,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_n,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[6]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -84,7 +99,26 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },	
+	/* media control key */
+	{ 0,                            XF86MonBrightnessDown, spawn, {.v = cmdbrightnessdown } },
+	{ 0,                            XF86MonBrightnessUp, spawn, {.v = cmdbrightnessup } },
+	{ 0,                            XF86AudioMute, spawn, {.v = cmdsoundtoggle } },
+	{ 0,                            XF86AudioRaiseVolume, spawn, {.v = cmdsoundup } },
+	{ 0,                            XF86AudioLowerVolume, spawn, {.v = cmdsounddown } },
+	{ 0,                            XF86AudioMicMute, spawn,	{.v = cmdMicOff } },
+	{ 0,                            XF86Display,			spawn,		{.v = screenSwitch } },
+	{ 0,                            XF86AudioNext,			spawn,		{.v = cmdnext } },
+	{ 0,                            XF86AudioPause,			spawn,		{.v = cmdplay } },
+	{ 0,                            XF86AudioPlay,			spawn,		{.v = cmdplay } },
+	{ 0,                            XF86AudioStop,			spawn,		{.v = cmdstop } },
+	{ 0,                            XF86AudioPrev,			spawn,		{.v = cmdprev } },
+	{ MODKEY,                       XK_F1,     spawn,          TERMCMD("ranger") },
+	{ MODKEY,                       XK_F6,     spawn,          {.v = Webcmd } },
+	{ MODKEY,                       XK_F7,     spawn,          {.v = Impcmd } },
+	{ MODKEY,                       XK_F8,     spawn,          {.v = Telegramcmd } },
+	{ MODKEY,                       XK_F9,     spawn,          {.v = mailcmd } },
+	{ MODKEY,                       XK_F10,    spawn,          {.v = Discordcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -94,7 +128,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ Mod1Mask|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
